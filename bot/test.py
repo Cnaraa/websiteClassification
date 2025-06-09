94ad3c9f1725a11a1bbb49c07c22dc463ada5db3
DANIIL <tsinaridze00@bk.ru>
2025-06-09 21:00:50 +0300

    delete test file

--- bot/test.py
+++
@@ -1,9 +0,0 @@
-from text_preprocessing import preprocess_text
-
-text = 'Привет! Это тестовый текст с лишними символами: 1234567890.'
-
-result = preprocess_text(text)
-
-print(f'Это исходный текст: {text}')
-print("-" *100)
-print(f'Это обработанный текст: {result}')
